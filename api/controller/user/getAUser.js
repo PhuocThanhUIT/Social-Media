58239d56bd9ab588b9a9a96a62a4cb2c316e67b6
Hứa Phước Thanh <17521046@ms.uit.edu.vn>
2022-03-05 17:01:16 +0700

    Topbar component

--- api/controller/user/getAUser.js
+++ api/controller/user/getAUser.js
@@ -3,7 +3,7 @@ const User = require('../../models/User');
 exports.getAUser = async (req, res) => {
     try {
       const user = await User.findById(req.params.id);
-      const { password, updatedAt, ...other } = user._doc;
+      const { password, updatedAt, isAdmin, ...other } = user._doc;
       res.status(200).json(other);
     } catch (err) {
       res.status(500).json(err);
